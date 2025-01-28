aa2c7a9bcc054ff3231f3cbe8827861fade06f13
Abrorbek <abrorbek27.05@gmail.com>
2025-01-28 18:30:57 +0500

    lesson 1

---
+++ lesson1_hw.py
@@ -0,0 +1,18 @@
+#001
+side =  int((input(": ")))
+print(f'perimeter: {side*4}')
+print(f'area: {side*side}')
+
+#002
+diameter = int(input(": "))
+print(f'The lengh is {3.14*diameter}')
+
+#003
+a = int(input("a: "))
+b = int(input("b: "))
+print(f'The mean is {(a+b)/2}')
+
+#004
+a = int(input("a: "))
+b = int(input("b: "))
+print(f'The square of a : {a*a}, b : {b*b}')
